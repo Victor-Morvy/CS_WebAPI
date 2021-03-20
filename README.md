443b861a7a295ad1925e0902530fba69c24515d4
Victor-Morvy <morvy0@gmail.com>
2021-03-20 02:28:30 -0300

    Commit only changed source

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # CS_WebAPI
 Simple .Net C# Web Api
+
+##Briefing
+This is a simple web api, i used the "ASP.NET Core web api" template.
---
+++ source/Controllers/ClientesController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using webApiCS.Models;
+
+namespace webApiCS.Controllers
+{
+    public class ClientesController : ApiController
+    {
+        private static List<Cliente> clientes = new List<Cliente>();
+
+        public List<Cliente> Get()
+        {
+            return clientes;
+        }
+
+        public void Post(string _name)
+        {
+            bool isNull = string.IsNullOrEmpty(_name);
+
+            if (!isNull)
+                clientes.Add(new Cliente(_name));
+        }
+
+        public void Delete(string nome)
+        {
+            clientes.RemoveAt(clientes.IndexOf(clientes.First(x => x.Nome.Equals(nome))));
+        }
+
+    }
+}
